9b6fea6c54f9334654817f3f00988bee87b775df
Rafał Mikołajun <root@rmweb.pl>
2024-08-02 20:49:49 +0200

    Added PHPDoc blocks in the class responsible for handling UUIDs in Sulu

--- src/Core/Infrastructure/Sulu/UuidDoctrineListBuilder.php
+++ src/Core/Infrastructure/Sulu/UuidDoctrineListBuilder.php
@@ -3,31 +3,43 @@
 namespace App\Core\Infrastructure\Sulu;
 
 use Sulu\Component\Rest\ListBuilder\Doctrine\DoctrineListBuilder;
+use Sulu\Component\Rest\ListBuilder\ListBuilderInterface;
 use Symfony\Component\Uid\AbstractUid;
 use Symfony\Component\Uid\Uuid;
 
 class UuidDoctrineListBuilder extends DoctrineListBuilder
 {
-    public function setIds($ids): self
+    /**
+     * @param string[]|int[]|null $ids
+     */
+    public function setIds($ids): ListBuilderInterface
     {
         return parent::setIds($this->mapUuids($ids));
     }
 
-    public function setExcludedIds($excludedIds): self
+    /**
+     * @param string[]|int[]|null $excludedIds
+     */
+    public function setExcludedIds($excludedIds): ListBuilderInterface
     {
         return parent::setExcludedIds($this->mapUuids($excludedIds));
     }
 
     /**
-     * @return string[]|int[]
+     * @return string[]|int[]|null
      */
-    protected function findIdsByGivenCriteria(): array
+    protected function findIdsByGivenCriteria(): ?array
     {
         $ids = parent::findIdsByGivenCriteria();
 
         return $this->mapUuids($ids);
     }
 
+    /**
+     * @param string[]|int[]|null $ids
+     *
+     * @return string[]|int[]|null
+     */
     private function mapUuids(?array $ids): ?array
     {
         if (is_null($ids)) {
@@ -37,6 +49,7 @@ class UuidDoctrineListBuilder extends DoctrineListBuilder
         return array_map(function ($id) {
             return match (gettype($id)) {
                 'string' => Uuid::isValid($id) ? Uuid::fromString($id)->toBinary() : $id,
+                // @phpstan-ignore-next-line
                 'object' => $id instanceof AbstractUid ? $id->toBinary() : $id,
                 default => $id,
             };
